9c9b9141dde19422fc3805bcbeccf393fef4a2c1
Aakash Gajjar <skyqutip@gmail.com>
2020-07-08 19:24:56 +0530

    [release] fix setup.py
    - remove setup_requires dependency

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@
 from setuptools import find_packages
 from setuptools import setup
 
-from snapchat_dl import __version__
 
+version = {}
+with open("snapchat_dl/version.py") as fp:
+    exec(fp.read(), version)
 
 with open("README.md") as readme_file:
     readme = readme_file.read()
@@ -18,7 +20,7 @@ test_requirements = ["pytest"].extend(requirements)
 
 setup(
     name="snapchat-dl",
-    version=__version__,
+    version=version["__version__"],
     description="Snapchat Public Stories Downloader.",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -27,7 +29,6 @@ setup(
     author_email="skyqutip@gmail.com",
     entry_points={"console_scripts": ["snapchat-dl=snapchat_dl.cli:main",],},
     include_package_data=True,
-    setup_requires=requirements,
     install_requires=requirements,
     test_suite="tests",
     tests_require=test_requirements,
--- snapchat_dl/__init__.py
+++ snapchat_dl/__init__.py
@@ -2,7 +2,6 @@
 
 __author__ = """Aakash Gajjar"""
 __email__ = "skyqutip@gmail.com"
-__version__ = "0.3.1"
 
 from snapchat_dl.snapchat_dl import SnapchatDL
 
